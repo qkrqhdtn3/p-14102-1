7ec7852e3ff8447f09a21e6d1b70870f6d1c9968
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-28 14:34:57 +0900

    6 feat: write posts

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -1,16 +1,24 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
+
     @GetMapping("/posts/write")
     @ResponseBody
-    public String write(){
+    public String showWrite(){
         return """
-                <form action="doWrite">
+                <form action="doWriete">
                     <input type="text" name="title" placeholder="제목"
                      value="안녕">
                     <br>
@@ -19,5 +27,17 @@ public class PostController {
                     <input type="submit" value="작성">
                 </form>
                 """;
+    }
+
+    @GetMapping("/posts/doWrite")
+    @ResponseBody
+    @Transactional
+    public String write(
+            @RequestParam(defaultValue = "") String title,
+            @RequestParam(defaultValue = "") String content
+    ){
+        Post post = postService.write(title, content);
+
+        return "%d번 게시글 생성".formatted(post.getId());
     }
 }
